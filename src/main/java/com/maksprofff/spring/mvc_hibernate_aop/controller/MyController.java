9ea5f3ef83900c969706b07944f334d519d2f443
Maksim Kholodnyakov <maksprofff@yandex.ru>
2022-12-18 23:47:08 +0300

    5.6 Удаление работника_commit

--- src/main/java/com/maksprofff/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/maksprofff/spring/mvc_hibernate_aop/controller/MyController.java
@@ -46,7 +46,12 @@ public class MyController {
         Employee employee = employeeService.getEmployee(id);
         model.addAttribute("employee",employee);
         return "employee-info";
+    }
 
+    @RequestMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam("empId") int id) {
+        employeeService.deleteEmployee(id);
+        return "redirect:/";
     }
 
 
