8af4be5c8b6fe6b11a40d926f562a9d702376a7c
Maksim Kholodnyakov <maksprofff@yandex.ru>
2022-12-18 15:48:52 +0300

    5.5 Изменение существующего работника_commit

--- src/main/java/com/maksprofff/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
+++ src/main/java/com/maksprofff/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -19,7 +19,7 @@ public class EmployeeDAOImpl implements EmployeeDAO{
     @Override
     public void saveEmployee(Employee employee) {
         Session session = sessionFactory.getCurrentSession();
-        session.save(employee);
+        session.saveOrUpdate(employee);
     }
 
     @Override
@@ -32,5 +32,11 @@ public class EmployeeDAOImpl implements EmployeeDAO{
         return allEmployees;
     }
 
+    @Override
+    public Employee getEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Employee employee = session.get(Employee.class,id);
 
+        return employee;
+    }
 }
